24e6b77f3c8eb2565b7ceff44a92ef5f9bed77a9
jaayeon <jaayeon5200@gmail.com>
2021-10-23 21:37:58 +0900

    test for every images

--- tester.py
+++ tester.py
@@ -60,111 +60,108 @@ def run_test(opt, img_list, gt_img_list):
     noise_avg_psnr = 0.0
     noise_avg_ssim = 0.0
     for img_idx, path in enumerate(zip(img_list,gt_img_list),1):
-        if img_idx % 4 :
-            pass
-        else:
-            img_path = path[0]
-            print('img_path : ', img_path)
-            gt_img_path = path[1]
-            print('gt_img_path ; ', gt_img_path)
-            start_time = time.time()
-            img_path = os.path.abspath(img_path)
-            img_name = 'out_'+os.path.basename(img_path)
-            concat_name = 'concat_'+os.path.basename(img_path)
-            num_test_img = int(num_total_img/4)
+        img_path = path[0]
+        print('img_path : ', img_path)
+        gt_img_path = path[1]
+        print('gt_img_path ; ', gt_img_path)
+        start_time = time.time()
+        img_path = os.path.abspath(img_path)
+        img_name = 'out_'+os.path.basename(img_path)
+        concat_name = 'concat_'+os.path.basename(img_path)
+        num_test_img = int(num_total_img/4)
 
-            print("[{}/{}] processing {}".format(num+1, num_test_img, os.path.abspath(img_path)))
+        print("[{}/{}] processing {}".format(num+1, num_total_img, os.path.abspath(img_path)))
 
-            input_img = imageio.imread(img_path)
-            gt_img = imageio.imread(gt_img_path)
-            input_img_shape = input_img.shape
+        input_img = imageio.imread(img_path)
+        gt_img = imageio.imread(gt_img_path)
+        input_img_shape = input_img.shape
 
-            #only for gray scale img
-            input_img_tensor = torch.from_numpy(input_img.reshape(1,1,input_img.shape[0], input_img.shape[1])).to(opt.device)
-            gt_img_tensor = torch.from_numpy(gt_img.reshape(1,1,gt_img.shape[0], gt_img.shape[1])).to(opt.device)
+        #only for gray scale img
+        input_img_tensor = torch.from_numpy(input_img.reshape(1,1,input_img.shape[0], input_img.shape[1])).to(opt.device)
+        gt_img_tensor = torch.from_numpy(gt_img.reshape(1,1,gt_img.shape[0], gt_img.shape[1])).to(opt.device)
 
-            if opt.test_patches:
+        if opt.test_patches:
+            """  
+            have to fix img2tensor
+            """
+            pad_ts = pad_tensor(input_img_tensor, opt.patch_size, opt.patch_offset)
+            input_tensor = mp.make_tensor_arr_patches(pad_ts, opt.patch_size, opt.patch_offset)
+        else : 
+            input_tensor = input_img_tensor
+
+
+        # else:
+        #     input_tensor = input_img
+        #     if len(img.shape) ==2:
+        #         img = img.reshape(1, 1, img.shape[0], img.shape[1])
+
+        input_tensor_shape = input_tensor.shape
+        input_tensor = input_tensor.type(torch.FloatTensor)
+
+        if opt.use_cuda:
+            input_tensor = input_tensor.to(opt.device)
+
+        with torch.no_grad(): #(b,c,h,w) or (1,1,h,w)
+            if opt.ensemble:
                 """  
-                have to fix img2tensor
+                ensemble code... i'll add it later
                 """
-                pad_ts = pad_tensor(input_img_tensor, opt.patch_size, opt.patch_offset)
-                input_tensor = mp.make_tensor_arr_patches(pad_ts, opt.patch_size, opt.patch_offset)
-            else : 
-                input_tensor = input_img_tensor
-
-
-            # else:
-            #     input_tensor = input_img
-            #     if len(img.shape) ==2:
-            #         img = img.reshape(1, 1, img.shape[0], img.shape[1])
-
-            input_tensor_shape = input_tensor.shape
-            input_tensor = input_tensor.type(torch.FloatTensor)
-
-            if opt.use_cuda:
-                input_tensor = input_tensor.to(opt.device)
-
-            with torch.no_grad(): #(b,c,h,w) or (1,1,h,w)
-                if opt.ensemble:
-                    """  
-                    ensemble code... i'll add it later
-                    """
-                    out_tensor = torch.zeros(input_tensor.shape)
-                    for i in range(input_tensor.size(0)):
-                        print("input_tensor[{}:{}].shape: {}".format(i, i+1, input_tensor[i:i+1].shape))
-                        out_tensor[i] = forward_ensemble(input_tensor[i:i+1], net=net, device=opt.device)
-                else:
-                    if 'rev' in opt.way:
-                        out_tensor,_ = net(input_tensor, param=param)
-                    else : 
-                        out_tensor = net(input_tensor, param=param)
-                # print("out_tensor:", out_tensor.size())
-
-                out_tensor = out_tensor.to(opt.device)
-
-                # out = out.reshape(img_dims)
-                # print("out.shape:", out.shape)
-
-            out_img_path = os.path.join(opt.test_result_dir, img_name)
-            concat_img_path = os.path.join(test_concat_dir, concat_name)
-            
-
-            if opt.test_patches:
-                out_tensor = mp.recon_tensor_arr_patches(out_tensor, input_img.shape[1], input_img.shape[0], opt.patch_size, opt.patch_offset)
-                print("out_img.shape:", out_tensor.shape)
-                out_img_tensor = unpad_tensor(out_tensor, opt.patch_offset, input_tensor_shape)
-                print("unpad out_img.shape:", out_img_tensor.shape)
+                out_tensor = torch.zeros(input_tensor.shape)
+                for i in range(input_tensor.size(0)):
+                    print("input_tensor[{}:{}].shape: {}".format(i, i+1, input_tensor[i:i+1].shape))
+                    out_tensor[i] = forward_ensemble(input_tensor[i:i+1], net=net, device=opt.device)
             else:
-                out_img_tensor = out_tensor
+                if 'rev' in opt.way:
+                    out_tensor,_ = net(input_tensor, param=param)
+                else : 
+                    out_tensor = net(input_tensor, param=param)
+            # print("out_tensor:", out_tensor.size())
 
-            noise_loss, noise_psnr, noise_ssim, batch_loss, batch_psnr, batch_ssim = calc_metrics(input_img_tensor, out_img_tensor, gt_img_tensor)
-            # print('nl : {:.8f}, np : {:.8f}, ol : {:.8f}, op : {:.8f}'.format(noise_loss, noise_psnr, batch_loss, batch_psnr))
-            
-            #only for gray scale img
-            if opt.use_cuda:
-                out_img = out_img_tensor[0,0,:,:].to('cpu').detach().numpy()
-            else : 
-                out_img = out_img_tensor[0,0,:,:].detach().numpy()
+            out_tensor = out_tensor.to(opt.device)
 
-            concat_img = np.concatenate((input_img, out_img, gt_img), axis=1)
+            # out = out.reshape(img_dims)
+            # print("out.shape:", out.shape)
 
-            num += 1
-            avg_loss += batch_loss
-            avg_psnr += batch_psnr
-            avg_ssim += batch_ssim
-            noise_avg_loss += noise_loss
-            noise_avg_psnr += noise_psnr
-            noise_avg_ssim += noise_ssim
+        out_img_path = os.path.join(opt.test_result_dir, img_name)
+        concat_img_path = os.path.join(test_concat_dir, concat_name)
+        
+
+        if opt.test_patches:
+            out_tensor = mp.recon_tensor_arr_patches(out_tensor, input_img.shape[1], input_img.shape[0], opt.patch_size, opt.patch_offset)
+            print("out_img.shape:", out_tensor.shape)
+            out_img_tensor = unpad_tensor(out_tensor, opt.patch_offset, input_tensor_shape)
+            print("unpad out_img.shape:", out_img_tensor.shape)
+        else:
+            out_img_tensor = out_tensor
+
+        noise_loss, noise_psnr, noise_ssim, batch_loss, batch_psnr, batch_ssim = calc_metrics(input_img_tensor, out_img_tensor, gt_img_tensor)
+        # print('nl : {:.8f}, np : {:.8f}, ol : {:.8f}, op : {:.8f}'.format(noise_loss, noise_psnr, batch_loss, batch_psnr))
+        
+        #only for gray scale img
+        if opt.use_cuda:
+            out_img = out_img_tensor[0,0,:,:].to('cpu').detach().numpy()
+        else : 
+            out_img = out_img_tensor[0,0,:,:].detach().numpy()
+
+        concat_img = np.concatenate((input_img, out_img, gt_img), axis=1)
+
+        num += 1
+        avg_loss += batch_loss
+        avg_psnr += batch_psnr
+        avg_ssim += batch_ssim
+        noise_avg_loss += noise_loss
+        noise_avg_psnr += noise_psnr
+        noise_avg_ssim += noise_ssim
 
 
-            print("** Test {:.3f}s => Image({}/{}): Noise Loss: {:.8f}, Noise PSNR: {:.8f}, Noise SSIM: {:.8f}, Loss: {:.8f}, PSNR: {:.8f}, SSIM: {:.8f}".format(
-                time.time() - start_time, img_idx, num_test_img, noise_loss.item(), noise_psnr.item(), noise_ssim.item(), batch_loss.item(), batch_psnr.item(), batch_ssim.item()
-            ))
+        print("** Test {:.3f}s => Image({}/{}): Noise Loss: {:.8f}, Noise PSNR: {:.8f}, Noise SSIM: {:.8f}, Loss: {:.8f}, PSNR: {:.8f}, SSIM: {:.8f}".format(
+            time.time() - start_time, img_idx, num_total_img, noise_loss.item(), noise_psnr.item(), noise_ssim.item(), batch_loss.item(), batch_psnr.item(), batch_ssim.item()
+        ))
 
-            # print("out_img.shape:", out_img.shape)
-            # print(os.path.abspath(out_img_path))
-            imageio.imwrite(concat_img_path, concat_img)
-            imageio.imwrite(out_img_path, out_img)
+        # print("out_img.shape:", out_img.shape)
+        # print(os.path.abspath(out_img_path))
+        imageio.imwrite(concat_img_path, concat_img)
+        imageio.imwrite(out_img_path, out_img)
 
     print(" #{:d} Test Average Noise Loss: {:.8f}, Average Noise PSNR: {:.8f}, Average Noise SSIM: {:.8f}, Average Loss: {:.8f}, Average PSNR: {:.8f}, Average SSIM: {:.8f}".format(
         num, noise_avg_loss / num, noise_avg_psnr / num, noise_avg_ssim / num, avg_loss / num, avg_psnr / num, avg_ssim / num
--- tests_upload/cut_pdf.py
+++ tests_upload/cut_pdf.py
@@ -13,17 +13,18 @@ if __name__ == "__main__":
     new_min = -160.0
     new_max = 240.0
 
-    test_dirs = ['ge-20210819-0418-rev-edsr-p-chest-pelvis-testset-mayo-1mm',
-                'ge-20210819-0413-rev-edsr-p-chest-pelvis-testset-mayo-1mm',
-                'ge-20210819-0414-rev-edsr-p-chest-pelvis-testset-mayo-1mm',
-                'ge-20210819-0425-rev-edsr-p-chest-pelvis-testset-mayo-1mm']
+    test_dirs = ['full_1mm',
+                'quarter_1mm']
     # test_dirs = ['1mm_nlm_eunji',
     #             '3mm_nlm_eunji']
-    test_path = '../../data/denoising/test_result_DA'
+    # test_path = '../../data/denoising/test_result_DA'
+    # save_path = '../../data/denoising/test_pdf'
+
+    test_path = '../../data/denoising/test/mayo'
     save_path = '../../data/denoising/test_pdf'
 
     for test_dir in test_dirs:
-        test_dir_path = os.path.join(test_path, test_dir, '*.tiff')
+        test_dir_path = os.path.join(test_path, test_dir, '*/*.tiff')
         test_imgs = glob.glob(test_dir_path)
         test_imgs.sort()
 
@@ -41,15 +42,4 @@ if __name__ == "__main__":
             save_dir_path = os.path.join(save_path, test_dir)
             os.makedirs(save_dir_path, exist_ok=True)
             imageio.imwrite(os.path.join(save_dir_path, os.path.basename(img_pth)), dc_img)
-
-            
-        #     0-1 normalization
-
-        # 저장
-        # psnr구해주고
-        # psnr
-        # [1,1,h,w] tensor 지정하고~ 뽑아줘~
-
-        # 노이즈 psnr..
-        # nlm psnr : 
 
